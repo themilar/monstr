ceb2612e59e9c27af4531a4b2d0abc3fdadaaade
Damilare Olanipekun <damilare@lag025ict.ngdelta.net>
2023-04-26 17:29:00 +0100

    created ticketes app

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,6 +80,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "monstr.users",
     # Your stuff: custom apps go here
+    "monstr.tickets"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
