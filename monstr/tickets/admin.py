f6e9459a5cdb2f83926af82e106d4bba8dedb86b
Damilare Olanipekun <damilare@Damilares-MacBook-Air.local>
2023-04-27 17:42:28 +0100

    created ticket and label models, and added them to admin

--- monstr/tickets/admin.py
+++ monstr/tickets/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ticket,Label
 
-# Register your models here.
+admin.site.register(Label)
+admin.site.register(Ticket)
--- monstr/tickets/models.py
+++ monstr/tickets/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
+from model_utils.models import TimeStampedModel
+from django.conf import settings
 
-# Create your models here.
+
+class Ticket(TimeStampedModel):
+    # category priority and status + as choices or related models
+
+    subject = models.CharField("Subject of the ticket", max_length=255)
+    content = models.TextField("Content", blank=True)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=False, on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return self.subject
+
+    # def get_absolute_url(self):
+    #     return reverse("ticket:detail",kwargs={"slug":self.name})
+
+
+class Label(models.Model):
+    class Category(models.TextChoices):
+        TYPE = "type", "Type"
+        STATUS = "status", "Status"
+        PRIORITY = "priority", "Priority"
+
+    category = models.CharField(max_length=8, choices=Category.choices)
+    name = models.CharField(
+        "Name of the category", max_length=20, default="unspecified",unique=True
+    )
+    colour = models.CharField("Associated colour", max_length=20, default="grey")
+    ticket = models.ForeignKey(
+        Ticket, null=True, on_delete=models.SET_NULL, related_name="labels"
+    )
